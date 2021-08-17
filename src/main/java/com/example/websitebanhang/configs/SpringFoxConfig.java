4a0647e34060f6d0f6918f875d8ae7f5f1842d46
thangtony <thangnv@mintpotvn.com>
2021-08-17 21:29:28 +0700

    Merge branch 'master' of https://github.com/thanglovemusic94/admin_theme_ace_spring_boot

---
+++ src/main/java/com/example/websitebanhang/configs/SpringFoxConfig.java
@@ -0,0 +1,22 @@
+package com.example.websitebanhang.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+@Configuration
+@EnableSwagger2WebMvc
+@Import(SpringDataRestConfiguration.class)
+public class SpringFoxConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
--- src/main/java/com/example/websitebanhang/search/ProductSearch.java
+++ src/main/java/com/example/websitebanhang/search/ProductSearch.java
@@ -1,9 +1,18 @@
 package com.example.websitebanhang.search;
 
 import lombok.Data;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+
+import java.util.List;
 
 @Data
 public class ProductSearch {
     private String name;
     private int price;
+
 }
+
+
